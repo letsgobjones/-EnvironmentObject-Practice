a27d39690859213a660ccc1fd3ba35a204aa45c6
Brandon <89342970+letsgobjones@users.noreply.github.com>
2024-06-19 13:42:17 +0900

    Completed Project.

--- EnvironmentObjectPractice/ContentView.swift
+++ EnvironmentObjectPractice/ContentView.swift
@@ -7,18 +7,55 @@
 
 import SwiftUI
 
+
+class AppState: ObservableObject {
+  //Pre iOS 17
+  @Published var isON: Bool = false
+  
+}
+
+
+
+
+struct LightBulbView: View {
+  
+  
+  @EnvironmentObject private var appState: AppState
+  var body: some View {
+    VStack {
+      Image(systemName: appState.isON ? "lightbulb.fill" : "lightbulb")
+        .font(.largeTitle)
+        .foregroundStyle(appState.isON ? .yellow : .black)
+      Button("Toggle") {
+        appState.isON.toggle()
+        
+      }
+
+  }
+}
+}
+
+struct LightRoomView: View {
+  var body: some View {
+    LightBulbView()
+  }
+}
+
+
 struct ContentView: View {
+  
+  @EnvironmentObject private var appState: AppState
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+          LightRoomView()
         }
         .padding()
+        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
+        .background(appState.isON ? .black : .white)
     }
 }
 
 #Preview {
     ContentView()
+    .environmentObject(AppState())
 }
